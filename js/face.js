0f38df704fcbef807358b2bea28f8f9f5e890fbc
Manuel Vigo <manuelvigo@Manuels-MacBook-Pro.local>
2018-08-02 13:05:37 -0500

    testingbw

--- js/face.js
+++ js/face.js
@@ -6,10 +6,10 @@ new BreathingHalftone( img, {
   dotSizeThreshold: 0.01,
   initVelocity: 0.7,
   oscPeriod: 2,
-  channels: [ 'red','green','blue','lum' ],
+  channels: [ 'lum' ],
   friction: 0.25,
-  hoverDiameter: 0.4,
-  hoverForce: 0.004,
+  hoverDiameter: 0.3.5,
+  hoverForce: -0.004,
   activeDiameter: -0.4,
   activeForce: 0.004
 });
